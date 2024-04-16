73e33997791a94d5e5cc2e8716f9604212930113
yanghao <792474530@qq.com>
2024-04-16 23:18:19 +0800

    不要这两个包

--- Chapter_3/1.py
+++ Chapter_3/1.py
@@ -1,8 +1,5 @@
 fruits = {'苹果': 2.5, '猕猴桃': 5, '西瓜': 1.9, '草莓': 10, '车厘子': 12}
-import pymysql
-print(pymysql.version_info)
+
 
 for a in fruits.items():
     print(a)
-import django
-print(django.get_version())
